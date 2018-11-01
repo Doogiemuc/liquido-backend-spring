3d8b336da264a2f0d7fb5e4f4c8d068ecd31e972
DoogieMuc <info@liquido.vote>
2018-11-01 14:29:00 +0100

    finally correct security config for H2 DB console

--- src/main/java/org/doogie/liquido/security/LiquidoH2ConsoleSecurityConf.java
+++ src/main/java/org/doogie/liquido/security/LiquidoH2ConsoleSecurityConf.java
@@ -7,22 +7,24 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 /**
- * Spring security configurtion to allow access to the H2 database web console
- * The H2 console also needs to be enabled in application.properties
+ * Spring security configuration to allow access to the H2 database web console
+ * The H2 console also needs to be enabled in application.properties:
+ * <pre>spring.h2.console.enabled=true</pre>
  */
 @Slf4j
 @Configuration
-@Order(-100)  // low value => very high priority!
+@Order(1)  // low value => very high priority!
 public class LiquidoH2ConsoleSecurityConf extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		log.trace("Configuring security for H2 DB console");
 		http
-			.antMatcher("/h2-console/**")
-				.headers().frameOptions().disable()  // needed for H2 DB console
-			.and()
-				.csrf().disable()
+			.antMatcher("/h2-console/**")   // everything below this path:
 				.authorizeRequests().anyRequest().permitAll()
+				.and()
+				.headers().frameOptions().disable()  // needed for H2 DB console
+				.and()
+				.csrf().ignoringAntMatchers("/h2-console/**")   // H2 console does not support CSRF protection
 		;
 	}
 }
