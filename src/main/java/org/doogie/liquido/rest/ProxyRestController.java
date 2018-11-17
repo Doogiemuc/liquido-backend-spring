e3360f519d6c524328235c9fff126e9985989c12
DoogieMuc <github@doogie.de>
2018-11-17 11:07:26 +0100

    Bugfix for returning plain string values from controllers. ALL TESTS ARE GREEN

---
+++ src/main/java/org/doogie/liquido/rest/ProxyRestController.java
@@ -0,0 +1,114 @@
+package org.doogie.liquido.rest;
+
+import lombok.extern.slf4j.Slf4j;
+import org.doogie.liquido.model.AreaModel;
+import org.doogie.liquido.model.DelegationModel;
+import org.doogie.liquido.model.UserModel;
+import org.doogie.liquido.rest.dto.AssignProxyRequest;
+import org.doogie.liquido.rest.dto.ProxyMapResponseElem;
+import org.doogie.liquido.security.LiquidoAuditorAware;
+import org.doogie.liquido.services.LiquidoException;
+import org.doogie.liquido.services.ProxyService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.rest.webmvc.BasePathAwareController;
+import org.springframework.data.rest.webmvc.PersistentEntityResourceAssembler;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.springframework.web.bind.annotation.RequestMethod.DELETE;
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
+import static org.springframework.web.bind.annotation.RequestMethod.PUT;
+
+/**
+ * Liquido REST endpoint for working with proxies.
+ */
+@Slf4j
+@BasePathAwareController  //ProxyRestController MUST be a BasePathAwareController!!! Otherwise the deserialization of URIs does not work in POST /my/proxy
+public class ProxyRestController {
+
+	@Autowired
+	ProxyService proxyService;
+
+	@Autowired
+	LiquidoAuditorAware liquidoAuditorAware;
+
+	/**
+	 * get all proxies that this user currently has assigned (per area)
+	 * @return a Map with directProxy and topProxy per area
+	 */
+	@RequestMapping(value = "/my/proxyMap", method = GET, produces = MediaType.APPLICATION_JSON_VALUE)
+	public @ResponseBody
+	Map getProxyMap() throws LiquidoException {
+		UserModel user = liquidoAuditorAware.getCurrentAuditor()
+				.orElseThrow(()-> new LiquidoException(LiquidoException.Errors.NO_LOGIN, "You must be logged in to get your proxy map!"));
+		Map<AreaModel, UserModel> proxyMap = proxyService.getProxyMap(user);
+		HashMap<String, Object> result = new HashMap<>();
+		for(AreaModel area: proxyMap.keySet()) {
+			UserModel directProxy = proxyMap.get(area);
+			UserModel topProxy = proxyService.findTopProxy(area, user);
+			ProxyMapResponseElem elem = new ProxyMapResponseElem(directProxy, topProxy);
+			result.put(area.getTitle(), elem);
+		}
+		return result;
+	}
+
+	/**
+	 * Save a proxy for the logged in user. This will insert a new delegation or update an existing one in that area.
+	 * @param assignProxyRequest assign proxy toProxy in area with voterToken
+	 * @return the created (or updated) delegation as HAL+JSON
+	 *         If you you need full detailed data of all referenced entities, you can request the delegationProjection.
+	 *         Or you can send additional requests for example for the "_links.toProxy.href" URI
+	 */
+	@RequestMapping(value = "/my/proxy", method = PUT)
+	@ResponseStatus(value = HttpStatus.CREATED)
+	public
+	ResponseEntity<?>                			// Return normal HTTP response with savedDelegation
+	//PersistentEntityResource 						// Return HAL representation of Model
+	//HttpEntity<DelegationProjection>    // This way one could return the DelegationProjection  (with inlined referenced objects)  but that cannot be used by the client for further updates
+	assignProxy(
+			@RequestBody AssignProxyRequest assignProxyRequest,
+			PersistentEntityResourceAssembler resourceAssembler
+			//Authentication auth  // not needed anymore - spring-security authentication object could be injected like this
+	) throws LiquidoException {
+		log.info("assignProxy("+assignProxyRequest+")");
+		UserModel currentUser = liquidoAuditorAware.getCurrentAuditor()
+				.orElseThrow(()-> new  LiquidoException(LiquidoException.Errors.NO_LOGIN, "Cannot save Proxy. Need an authenticated user."));
+
+		DelegationModel savedDelegation = proxyService.assignProxy(assignProxyRequest.getArea(), currentUser, assignProxyRequest.getToProxy(), assignProxyRequest.getVoterToken());
+		if (savedDelegation == null) {
+			log.info("Proxy is not yet assigned. Proxy must still confirm.");
+			return new ResponseEntity(HttpStatus.ACCEPTED);  // 202
+		} else {
+			log.info("Assigned new proxy");
+			return new ResponseEntity<>(savedDelegation, HttpStatus.CREATED);  // 201
+		}
+
+		// Return HATEOS representation of Delegation => does not work correctly, cause delegationRepo is not exposed as spring-data-rest endpoint.
+		//return resourceAssembler.toResource(savedDelegation);
+
+		// You should not return a DelegationProjection here, as this code would:
+		//   return new ResponseEntity<>(savedDelegation, HttpStatus.OK);
+		// because that cannot be used for further updates by the client.
+		// http://stackoverflow.com/questions/30220333/why-is-an-excerpt-projection-not-applied-automatically-for-a-spring-data-rest-it
+	}
+
+	/**
+	 * delete a proxy of the currently logged in user in one area.
+	 * @param area the area where to delete the proxy
+	 * @return HTTP status 204 (NoContent) on success
+	 */
+	@RequestMapping(value = "/my/proxy/{areaId}", method = DELETE)
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void deleteProxy(@PathVariable(name="areaId") AreaModel area) throws LiquidoException {
+		UserModel currentUser = liquidoAuditorAware.getCurrentAuditor()
+				.orElseThrow(() -> new LiquidoException(LiquidoException.Errors.NO_LOGIN, "Need login to delete proxy!"));
+		log.info("deleteProxy(voter="+currentUser+", area="+area+")");
+		proxyService.removeProxy(area, currentUser, currentUser.getPasswordHash());
+	}
+
+}
