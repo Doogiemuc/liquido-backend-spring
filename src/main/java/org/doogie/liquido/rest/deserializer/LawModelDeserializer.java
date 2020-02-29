c3f8eae085b18a7f3533982178881d9fe1a6f8c3
DoogieMuc <github@doogie.de>
2020-02-29 08:51:47 +0100

    GREEN Backend tests. Added testCreateNowPoll

--- src/main/java/org/doogie/liquido/rest/deserializer/LawModelDeserializer.java
+++ src/main/java/org/doogie/liquido/rest/deserializer/LawModelDeserializer.java
@@ -5,7 +5,7 @@ import org.doogie.liquido.model.LawModel;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.jackson.JsonComponent;
 
-//@JsonComponent I cannot add this, because then these Entity Deserializers are used everywhere, e.g. in @RepositoryRestResource calls
+//@JsonComponent // => I cannot add this, because then these Entity Deserializers are used everywhere, e.g. in @RepositoryRestResource calls
 // So therefor I have to manually configure these deserializers where they are needed with @JsonDeserialize annotation
 // https://www.baeldung.com/spring-boot-jsoncomponent
 public class LawModelDeserializer extends EntityDeserializer<LawModel> {
