98510fe4eaf4278bb9b544351da1c523c574bd00
DoogieMuc <info@liquido.vote>
2021-11-30 11:55:58 +0100

    feat: Proposals with icons

--- src/main/java/org/doogie/liquido/testdata/TestDataUtils.java
+++ src/main/java/org/doogie/liquido/testdata/TestDataUtils.java
@@ -59,7 +59,7 @@ public class TestDataUtils {
 	@Autowired
 	JdbcTemplate jdbcTemplate;
 
-	Random rand = new Random();
+	private static Random rand = new Random();
 
 	public Map<String, UserModel> usersMap = new HashMap();
 	public List<UserModel> users = new ArrayList();
@@ -250,7 +250,14 @@ public class TestDataUtils {
 		auditorAware.setMockAuditor(createdBy);
 		LawModel proposal = upsert(new LawModel(title, description, area));
 
-		proposal = addSupportersToIdea(proposal, prop.supportersForProposal);
+		proposal.setIcon(getRandomFaIcon());
+
+		// Add enough supporters so that the idea becomes a proposal. Or immediately create proposal if ideas are disabled (for mobile client)
+		if (prop.supportersForProposal > 0) {
+			proposal = addSupportersToIdea(proposal, prop.supportersForProposal);
+		} else {
+			proposal.setStatus(LawModel.LawStatus.PROPOSAL);
+		}
 		LocalDateTime reachQuorumAt = LocalDateTime.now().minusDays(reachedQuorumDaysAgo);
 		proposal.setReachedQuorumAt(reachQuorumAt);      // fake reachQuorumAt date to be in the past
 
@@ -260,6 +267,14 @@ public class TestDataUtils {
 		return proposal;
 	}
 
+	private static String[] faIcons = {"chair", "chalkboard", "chalkboard-teacher", "charging-station", "chart-area", "chart-bar",
+		"magic", "magnet", "mail-bulk", "male", "map", "map-marked", "map-marked-alt", "map-marker",
+		"tag", "tags", "tape", "tasks", "taxi", "teeth", "teeth-open", "temperature-high"};
+
+	public static String getRandomFaIcon() {
+		int idx = rand.nextInt(faIcons.length);
+		return faIcons[idx];
+	}
 
 	private static final String loremIpsum = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, nam urna. Vitae aenean velit, voluptate velit rutrum. Elementum integer rhoncus rutrum morbi aliquam metus, morbi nulla, nec est phasellus dolor eros in libero. Volutpat dui feugiat, non magna, parturient dignissim lacus ipsum in adipiscing ut. Et quis adipiscing perferendis et, id consequat ac, dictum dui fermentum ornare rhoncus lobortis amet. Eveniet nulla sollicitudin, dolore nullam massa tortor ullamcorper mauris. Lectus ipsum lacus.\n" +
 		"Vivamus placerat a sodales est, vestibulum nec cursus eros fermentum. Felis orci nunc quis suspendisse dignissim justo, sed proin metus, nunc elit ac aliquam. Sed tellus ante ipsum erat platea nulla, enim bibendum gravida condimentum, imperdiet in vitae faucibus ultrices, aenean fringilla at. Rhoncus et sint volutpat, bibendum neque arcu, posuere viverra in, imperdiet duis. Eget erat condimentum congue ipsam. Tortor nostra, adipiscing facilisis donec elit pellentesque natoque integer. Ipsum id. Aenean suspendisse et eros hymenaeos in auctor, porttitor amet id pellentesque tempor, praesent aliquam rhoncus convallis vel, tempor fusce wisi enim aliquam ut nisl, nullam dictum etiam. Nisi accumsan augue sapiente dui, pulvinar cras sapien mus quam nonummy vivamus, in vitae, sociis pede, convallis mollis id mauris. Vestibulum ac quis scelerisque magnis pede in, duis ullamcorper a ipsum ante ornare.\n" +
